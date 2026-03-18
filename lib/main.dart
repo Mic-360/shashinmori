48097d50b00b267b8863ee50188919c325f384ca
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2026-03-18 16:14:28 +0530

    Remove dotenv dependency and related code from the project. Update pubspec.yaml for consistency in formatting.
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/router.dart';
@@ -9,7 +8,6 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: '.env');
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
